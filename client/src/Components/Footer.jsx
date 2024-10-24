622ff3017f7eef21b088146adb6bc00474301e26
DevFaith <waitherafaith@outlook.com>
2024-10-24 23:20:54 +0300

    * feat(Footer.jsx): add Footer component to client/src/Components directory

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-blue-500 p-4 text-white text-center">
+        <p>&copy; 2024 M-Pesa Integration</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
