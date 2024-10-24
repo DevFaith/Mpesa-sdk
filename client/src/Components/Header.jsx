59b6581d5f45c68aaa50661bd342cd4f14aed924
DevFaith <waitherafaith@outlook.com>
2024-10-24 23:21:01 +0300

    * feat(Header.jsx): add Header component to display the title of the M-Pesa Integration App

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header className="bg-blue-500 p-4 text-white">
+        <h1 className="text-xl">M-Pesa Integration App</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
